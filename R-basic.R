5b7e3d6e4db67ab0ff5a090f263ab9b00cd9c309
koeunmio <koeunmio@hanmail.net>
2020-01-04 15:12:51 +0900

    R basic grammar

--- R-basic.R
+++ R-basic.R
@@ -25,12 +25,57 @@ write.table(cars, "clipboard")
 "
 ▶ R의 기본 기능
 "
-summary(cars)
-head(cars, n=10)
-tail(cars)
+summary(cars)               # cars.info()
+head(cars, n=10)            # cars.head(10)
+tail(cars)                  # cars.tail()
 
-rownames(cars)
-colnames(cars)
+rownames(cars)              # cars.rownames
+colnames(cars)              # cars.colnames
 
 cars$model = rownames(cars) # rownames(cars) = index
 rownames(cars) = NULL       # index 제거
+
+cars$maker = word(cars$model, 1) # cars.split(" ", 1)
+
+# cars[cars['cyl'] == 4, ["maker", "model", "mpg", "cyl"]]
+cars.small.narrow = 
+  cars %>%
+  filter(cyl == 4) %>%
+  select(maker, model, mpg, cyl) 
+
+# pd.pivot_table(cars, index=['maker'], columns=['mpg'], aggfunc=np.mean)
+# pd.pivot_table(cars, index=['maker'], aggfunc={ 'mpg':np.mean })
+makers = 
+  cars %>%
+  group_by(maker) %>%
+  summarize(maker.mpg = mean(mpg)) 
+
+# pd.merge(cars, makers, on='maker', how='left')
+cars.makers = merge(cars, makers, by = "maker")
+
+
+"
+▶ R의 분석 기능
+"
+# 요약 통계
+table(cars$cyl) # cars['cyl'].describe()
+table(cars$gear, cars$cyl)
+
+# import matplotlib.pyplot as plt
+# plt.hist(cars['mpg'])
+# plt.show()
+hist(cars$mpg)
+
+# plt.scatter(cars['wt'], cars['mpg'])
+plot(cars$wt, cars$mpg)
+
+qplot(wt, mpg, data=cars, shape=factor(cyl))
+
+
+
+
+
+
+
+
+
